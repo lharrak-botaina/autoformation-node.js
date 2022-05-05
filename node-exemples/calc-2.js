49faccd06e996bead636eaa48f5885e607a777fa
lharrak-botaina <lharrak.botaina@gmail.com>
2022-05-05 13:22:52 +0000

    add calc.exmple

---
+++ node-exemples/calc-2.js
@@ -0,0 +1,6 @@
+var arguments = process.argv
+function add(a,b,c){
+    return parseInt(a)+parseInt(b)+parseInt(c)
+}
+var sum = add(arguments[2],arguments[3],arguments[4])
+console.log("the sum is :",sum )
